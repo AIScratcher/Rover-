bc7062d47a6bd7456bbded2b771667adebafa9ff
AIScratcher <34243678+AIScratcher@users.noreply.github.com>
2017-12-11 18:21:19 +0100

    11.12.17
    -Connection.ino
    +connectio.py

---
+++ Server/Connection.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 16:51:25 2017
+
+@author: SFZ_Laptop_012
+"""
+
--- Server/NeuronnalNetwork.py
+++ Server/NeuronnalNetwork.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec  4 16:58:13 2017
+At: SFZ HH
 
 @author: SFZ_Laptop_012
 """
 
 import numpy as np
+import Neuron
 
 class NeuronalNetwork(object):
 
     def __init__(inputs_shape,outputs_shape):
-        self.inputs = np.zeros(inputs_shape,dtype=object)
-        self.outputs = np.zeros(outputs_shape,dtype=object)
+        
+        
