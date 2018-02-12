0afc680c1d5d8e77d9a61f10fbacc335305cd78b
AIScratcher <34243678+AIScratcher@users.noreply.github.com>
2018-02-12 19:00:00 +0100

    Architektur
    Erste Fortschritt bei der Umsetzung der Architektur des   Netzwerks. Genaueres in den Notizen.

--- Rover/Camera/Camera.ino/Camera.ino.ino
+++ Rover/Camera/Camera.ino/Camera.ino.ino
@@ -1,13 +1,14 @@
 #include "SoftwareSerial.h"
 #include <VC0706_UART.h>
 #include <SPI.h>
-//#define SS_SD  10
+#define SS_SD  10
 bool canSnap = false;
 //use software serial
 SoftwareSerial cameraconnection(2,3);//Rx, Tx
 VC0706 cam = VC0706(&cameraconnection);
-//use hardware serial
-//VC0706 cam = VC0706(&Serial1);
+
+
+typedef unsigned int  uint;
 
 void setup() 
 {
@@ -54,17 +55,15 @@ void snapShot()
     int32_t time = millis();
     cam.getPicture(jpglen);
     uint8_t picture[jpglen];
-    uint8_t *buffer;
-    while(jpglen != 0){
-         uint8_t bytesToRead = min(32, jpglen);
-         buffer = cam.readPicture(bytesToRead);     
-         picture[jpglen] = buffer;
-         jpglen -= bytesToRead;   
-    } 
-
-    for(int i = 0;i < picture.length();i++) {
+    uint8_t *buffer = cam.readPicture(jpglen);
+    for(int i = 0;i < jpglen;i++) {
+     picture[i] = buffer[i];
+    }
+    for(int i = 0;i < jpglen;i++) {
       Serial.print(picture[i],DEC);
     }
     cam.resumeVideo();    
 }
+
+
 
