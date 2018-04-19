fa281588c6e4677d48da508c616df26a4692b41b
aiscratch <aiscratcher@gmx.de>
2018-04-19 06:52:47 +0200

    Perfome changes for a better documentation. Added Server/README.html and finished the Neuron class in Layer.py. TODO Test of the Neuron class.

--- Server/Layer.py
+++ Server/Layer.py
@@ -27,7 +27,8 @@ class Neuron(object):
     def __init__(self,input_f,input_c,input_b,
                 theta=np.random.uniform(0.01),
                 psi=np.random.uniform(0.01)):
-                self.theta, self.psi = theta,psi
+                self.theta, self.psi,self.tresh_pool_c = theta,psi,
+                np.random.uniform(0.01)
                 #Define inputs and fill it temporaly
                 #with the whole inhibition area
                 self.inputs = [input_f + input_c + input_b]
@@ -53,11 +54,35 @@ class Neuron(object):
     def r_predicting(feedforward_w,feedforward_n):
         if feedforward_w / feedforward_n > self.theta: #First Treshold
             self.output = 1
+        else:
+            self.output = 0
+        return
     #2.Context pooling
     def r_c_pooling(context_w,context_n):
-
+        #This will only execute if the output is 1
+        #Pooling is the process of learning a specific template in the inputs
+        #First test if the output and the context are good
+        if context_w / context_n > self.tresh_pool_c:
+            #The Context said: Yes we also think thats rigth what you said
+            #You can try to decrement a little
+            self.theta -= 0.045 #Random number but smaller as the number in else
+        else:
+            #The Context said you are wrong you must be more critical:
+            #Increment the theta
+            self.theta += 0.1
     #3.Feedback pooling
     def r_b_pooling(feedback_w,feedback_n):
+        #Do the same but now work with the tresh_pool_c
+        #Now we use a const, the lambada, I'm not lucky with this solution
+        #but before this will work in a large scale that and many more things
+        #in the theory must be fixed
+        #Again that will just execute if the output is 1
+        if feedback_w / feedback_n > self.lambada:
+            #Again slow down to a perfect value and never stand at one point
+            self.tresh_pool_c -= 0.034
+        else:
+            self.tresh_pool_c += 0.7
+        #Feedback Pooling is slower and not so fast changing how the context
 
 
 
