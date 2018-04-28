bf3d3434f9885cdacca2f8cd2dfb63fd6cd59b7c
aiscratch <aiscratcher@gmx.de>
2018-04-28 13:33:40 +0200

    Debug.py In the Debug.py I will write all classes I need for testing and debugging

--- Server/Debug.py
+++ Server/Debug.py
@@ -2,5 +2,10 @@ import numpy as np
 
 class psd_layer(object):
 
-    def __init__(self, arg):
-        
+    def __init__(self, size):
+        #size = total length of the Layer
+        self.__size = size
+    def size(self):
+        return self.__size
+    def get_neurons(self):
+        return np.zeros(self.__size)
