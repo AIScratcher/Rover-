e8d6860dc2c8e0ec6fc66d6f6b7503fae4108ccc
AIScratcher <34243678+AIScratcher@users.noreply.github.com>
2018-05-15 17:44:20 +0200

    New Function in Matrix.c: To better use Columns (dims) of a Matrix I added the functions getDim and setDim. These return or set a Column of a Matrix.

--- Layer.c
+++ Layer.c
@@ -1,6 +1,6 @@
 /*
  *	Author: Joris Gutjahr
- *	A Layer receives a Matrix as input an 
+ *	A Layer receives a Matrix as input an
  *	computes the binary output as Matrix.
  *
  */
@@ -9,13 +9,14 @@ typedef struct Layer {
 	int size; //Number of Neurons
 	Matrix (*run)(Matrix,struct Layer);//Run of the Layer
 	Matrix values; //Matrix with all variables for the Neurons
+    Matrix net;
 };
 typedef struct Layer Layer;
 Matrix Layer_run(Matrix,Layer);
 Matrix Layer_run(Matrix,Layer);
 
 
-struct Layer Layer_setup(int size) {
+struct Layer Layer_setup(int size,Matrix net) {
 	/*
 	 * 	It will return l
 	 * 	and hold  a Matrix with the three
@@ -24,20 +25,17 @@ struct Layer Layer_setup(int size) {
 	Layer l;
 	l.size = size;
 	l.run = &Layer_run;
-	l.values = Matrix_setup(3,size);//Layer view on the Network
+	l.values = Matrix_setup(3+2*((int)net.total_size - net.total_size/50)+(int))net.total_size/50+1,size);///Layer view on the Network
+    l.net = net; ///Network Matrix
 	return l;
 }
 
-Matrix Layer_run(Matrix input,Layer l) {
-	/*
-	 *	 The input:
-	 *	 2D Matrix with the variables
-	 *	 n and w for each  input
-	 */
-	//The second aren't set now
-	int outputs[l.size];//Output Array
-	for(int i = 0;i < l.size;i++) {
-		//Each column is a Neurons tresholds
-		
-	}
+void Load_Layer(size_t start,size_t end,Matrix net,Layer l) {
+    ///start:  first column thats part of the Layer
+    ///end: last column thats part of the Layer
+    ///net: Network Matrix
+    ///l: Layer struct
+    for(size_t i = start;i <= end;i++) {
+       l.values.setDim(i-start,net.getDim(i,net),l.values); ///Move whole column at once
+    }
 }
--- f_math.c
+++ f_math.c
@@ -1,24 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-
-struct random {
-        double x;//PI , can be every number;
-	int a; //Coming from a very low number (-10) to (+10);
-	int b; //Coming from a very high number (+500) to (-500); (Overflow is wanted)
-};
-struct random rnd;
-void init_random() {
-	rnd.x = 3.1415;
-	rnd.a = -10;
-	rnd.b = +500;
-}
-double random() {
-	rnd.a += 33;
-	rnd.b += 33;
-	rnd.a *= -1;
-	rnd.b *= -1;
-	return rnd.a * rnd.x + rnd.b;
-}
-double random_inrange(double begin,double end) {
-	return end-random() +begin;
+int main(void) {
+srand(time(NULL));
+//Seed
+int r = rand() % 2;
+printf("%d",r);
 }
