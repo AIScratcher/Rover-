e4992a6b3c4b80b390ee6d4c391046f64d0ef2b2
AIScratcher <34243678+AIScratcher@users.noreply.github.com>
2018-05-22 18:52:05 +0200

    Try to Debug LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -1,25 +1,24 @@
 #include <stdlib.h>
 typedef struct Node {
 	int key;
-	struct Node* next;
+	struct Node* next; //Next Node 
 } Node;
 
 typedef struct List {
-	int size;
-	Node* head;
-	Node* tail;
-	void (*append)(int,struct List);
-	int (*remove)(struct List);
+	Node* head; //First Node
+	Node* tail; //Last Node
+	void (*append)(int,struct List); //Add a   new node
+	int (*remove)(struct List); //Remove the Node at head
 } List;
 
 void List_append(int key,List l);
 int List_remove(List l);
 
 void List_append(int key,List l) {
-	Node new_Node;
-	new_Node.key = key;
-	l.tail->next = &new_Node;
-	l.tail = &new_Node;
+	Node* new_Node = (Node*) malloc(sizeof(Node));
+	new_Node->key = key;
+	l.tail->next = new_Node;
+	l.tail = new_Node;
 }
 int List_remove(List l) {
 	int key = l.head->key;
@@ -29,10 +28,11 @@ int List_remove(List l) {
 
 List setup_list() {
 	List l;
-	Node h;
-	h.key = 0;
-	h.next = 0;
-	l.head = &h;
-	l.tail = &h;
+	Node* first_node = (Node*) malloc(sizeof(Node));
+	first_node->key = 0;
+	l.head = first_node;
+	l.tail = first_node;
+	l.append = &List_append;
+	l.remove = &List_remove;
 	return l;
 }
